2cf03372326f09e4603eb18984a7e505d409e91a
Jon D Jones <80398615+jondjones-poc@users.noreply.github.com>
2022-11-12 20:36:19 +0000

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,35 +1,11 @@
-var start = process.hrtime();
-
-var elapsed_time = function(note){
-
-    var precision = 3;
-    var elapsed = process.hrtime(start)[1] / 1000000;
-    var time = process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note;
-
-    console.log(time);
-
-    start = process.hrtime();
-
-    return time;
-}
-
-var functionOne  = function(){
-    var result = 1 + 1;
+export const config = {
+  runtime: 'experimental-edge',
 }
 
 export default function handler(request, response) {
     var infoObject = {};
 
-    elapsed_time('start Function #1');
-
-    var iterations = 1000000000;
-    for(var i = 0; i < iterations; i++ ){
-        functionOne();
-    };
-
-    infoObject.end = elapsed_time('end Function #1');
-
     response.status(200).json({
-        body: JSON.stringify(infoObject)
+        body: "Test"; 
       });
   }
